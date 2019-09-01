bfdb3c042e148bdfbb267d148f9f87eab31ac831
MAwais <mawais54013@outlook.com>
2019-09-01 12:01:53 -0700

    Added Timer and Seekbar

--- app/src/main/java/com/muhammadawais/timer/MainActivity.java
+++ app/src/main/java/com/muhammadawais/timer/MainActivity.java
@@ -2,19 +2,73 @@ package com.muhammadawais.timer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+    Boolean counterIsActive = false;
+    Button goButton;
+
+    public void buttonClicked(View view) {
+
+        if (counterIsActive) {
+
+            timerTextView.setText("0:30");
+            timerSeekBar.setProgress(30);
+            
+
+        } else {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("STOP!");
+
+            new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long l) {
+                    updateTimer((int) l / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alert);
+                    mediaPlayer.start();
+                }
+
+            }.start();
+        }
+     }
+
+     public void updateTimer(int secondsLeft) {
+         int minutes = secondsLeft / 60;
+         int seconds = secondsLeft - (minutes * 60);
+
+         String secondString = Integer.toString(seconds);
+
+         if(seconds <= 9)
+         {
+             secondString = "0" + secondString;
+         }
+
+         timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = findViewById(R.id.countDown);
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.countDown);
+        goButton = findViewById(R.id.startButton);
 
 //        set max of 60 min and current of 30 seconds
         timerSeekBar.setMax(600);
@@ -24,18 +78,7 @@ public class MainActivity extends AppCompatActivity {
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-                int minutes = i / 60;
-                int seconds = i - (minutes * 60);
-
-                String secondString = Integer.toString(seconds);
-
-                if(secondString == "0")
-                {
-                    secondString = "00";
-                }
-
-                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+                updateTimer(i);
             }
 
             @Override
