1ab14a85c81f904981e70dae28311e557d13672b
MAwais <mawais54013@outlook.com>
2019-08-31 22:28:49 -0700

    Added Functions

--- app/src/main/java/com/muhammadawais/timer/MainActivity.java
+++ app/src/main/java/com/muhammadawais/timer/MainActivity.java
@@ -3,6 +3,8 @@ package com.muhammadawais.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        final TextView timerTextView = findViewById(R.id.countDown);
+
+//        set max of 60 min and current of 30 seconds
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+//        change listener
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int minutes = i / 60;
+                int seconds = i - (minutes * 60);
+
+                String secondString = Integer.toString(seconds);
+
+                if(secondString == "0")
+                {
+                    secondString = "00";
+                }
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
